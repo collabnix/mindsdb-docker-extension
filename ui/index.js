31640ae90b12b92e062b58f62b5bc8daa135cf9c
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-02-12 10:52:06 +0530

    added a function to check if server is ready

---
+++ ui/index.js
@@ -0,0 +1,16 @@
+function checkServer() {
+    fetch('http://localhost:47334')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Server not ready');
+        }
+        document.getElementById('loader').style.display = 'none';
+        window.location.href = 'http://localhost:47334';
+      })
+      .catch(error => {
+        // Server not ready yet. Wait for 2 seconds and try again
+        setTimeout(checkServer, 2000);
+      });
+  }
+
+  window.onload = checkServer;
